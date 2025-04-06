3083b88612b8280b09bed08ccd977ec2ac7c2761
BlindMuadDib <jccrandall14@gmail.com>
2025-04-06 11:42:03 -0400

    updated separate endpoint

--- musictranslator/musicprocessing/separate.py
+++ musictranslator/musicprocessing/separate.py
@@ -1,22 +1,32 @@
 """
-Uses Spleeter by Deezer to isolate each instrument track
+Uses Demucs to isolate each instrument track
 Allows more accurate lyric alignment and volume/pitch analysis
 """
     # Licensed under MIT license.
-    # Repository: https://github.com/deezer/spleeter
+    # Repository: https://github.com/adefossez/demucs
 import requests
 
-SPLEETER_SERVICE_URL = "http://spleeter_wrapper:22227/split"
+SEPARATOR_SERVICE_URL = "http://separator_wrapper:22227/separate"
 
 def split_audio(input_file):
-    """ Temporarily opens the audio for Kubernetes """
+    """
+    Sends the audio file path to the separator wrapper for processing
+    Returns a dictionary of separated audio file paths
+    """
     try:
-        with open(input_file, 'rb') as audio_file:
-            files = {'audio': audio_file}
-            response = requests.post(SPLEETER_SERVICE_URL, files=files, timeout=10)
-            response.raise_for_status()
-            # The wrapper service has now created the files
-            return True
+        # Send only the filename, not the file content
+        data = {"audio_filename": os.path.basename(input_file)}
+        headers = {'Content-Type': 'application/json'}
 
+        response = requests.post(SEPARATOR_SERVICE_URL, json-data, headers=headers, timeout=10)
+        response.raise_for_status()
+        # The wrapper service has now created the files
+        return response.json()
+
+    except requests.exceptions.HTTPError as e:
+        try:
+            return response.json()
+        except:
+            return {'error': f'Demucs HTTP Error: {e}'}
     except Exception as e: # pylint: disable=broad-except
-        return {'error': f'Spleeter Error: {e}'}
+        return {'error': f'Demucs Error: {e}'}
